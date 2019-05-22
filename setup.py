035ab083ffc092d5fe31b40318d90d67ae10166a
elderBi <30739836+elderBi@users.noreply.github.com>
2019-05-22 09:07:22 +0800

    add file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from cx_Freeze import setup, Executable
+import sys
+import os
+
+os.environ['TCL_LIBRARY'] = r"C:\Users\Bi001\AppData\Local\Programs\Python\Python36\tcl\tcl8.6"
+os.environ['TK_LIBRARY'] = r"C:\Users\Bi001\AppData\Local\Programs\Python\Python36\tcl\tk8.6" 
+
+include_files = [r"C:\Users\Bi001\AppData\Local\Programs\Python\Python36\DLLs\tcl86t.dll",
+				 r"C:\Users\Bi001\AppData\Local\Programs\Python\Python36\DLLs\tk86t.dll"]
+base = 'Win32GUI' if sys.platform == 'win32' else None
+options = {'packages': ['cn2an', 'tkinter', 're', 'winreg', 'os'], "include_files": include_files}
+
+setup(name = '<txtScissor>',
+          version = "1.0",
+          description = '<for my cat>',
+          options = {'build_exe': options},
+          executables = [Executable(script = 'txtScissor.py', 
+									base = base, 
+									targetName = 'txtScissor.exe', 
+									icon = 'cat.ico', )])
